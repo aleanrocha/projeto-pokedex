60bda6efc1f5536a6fe70c6ce85727763158a8e3
aleanrocha <alean.oficiall@gmail.com>
2023-03-16 11:45:46 -0300

    adicionando pokemons com js

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+const url = "https://pokeapi.co/api/v2/pokemon?limit=10"
+
+fetch(url)
+.then(response => response.json())
+.then(allpokemons => {
+
+  let pokemons = []
+
+  allpokemons.results.map((val) => {
+
+    fetch(val.url)
+    .then(response => response.json())
+    .then(pokemonSingle => {
+
+
+      pokemons.push({
+        nome:val.name,
+        imagem:pokemonSingle.sprites.front_default
+      })
+
+      
+
+      if (pokemons.length == 10) {
+        // finalizamos nossa requisições
+        let pokemonBoxe = document.querySelector("#pokemons")
+        pokemonBoxe.innerHTML = ""
+        
+        pokemons.map((val) => {
+          pokemonBoxe.innerHTML += `
+          <div class="pokemon-box">
+            <img src="${val.imagem}" alt="imagem do dito">
+            <p>${val.nome}</p>
+          </div> 
+          `
+        })
+
+      }
+    })
+
+  })
+})
